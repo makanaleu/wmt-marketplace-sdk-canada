12cd11ab68d59ad7a8fab8f055626231cb9cb993
Kane McConnell <kmcconnell@llpgroup.io>
2023-07-14 15:21:38 +0200

    Add currentTrackingInfo to shipment.ts

--- lib/orders/shipment.d.ts
+++ lib/orders/shipment.d.ts
@@ -59,6 +59,17 @@ export declare class OrderLineStatuses {
     constructor(ols: OrderLineStatuses);
 }
 /**
+ * Once the shipment is done, to update tracking details in the PO,
+ * you must pass the tracking number to be updated under the new
+ * currentTrackingInfo field.
+ */
+export declare class CurrentTrackingInfo {
+    /**
+     * The shipment tracking number
+     */
+    trackingNumber: string;
+}
+/**
  * Details about the OrderLine status.
  */
 export declare class OrderLineStatus {
@@ -74,5 +85,11 @@ export declare class OrderLineStatus {
      * Information about the package shipment and tracking updates.
      */
     trackingInfo: OrderLineTrackingInfo;
+    /**
+     * Once the shipment is done, to update tracking details in the PO,
+     * you must pass the tracking number to be updated under the new
+     * currentTrackingInfo field.
+     */
+    currentTrackingInfo: CurrentTrackingInfo;
     constructor(ols: OrderLineStatus);
 }
